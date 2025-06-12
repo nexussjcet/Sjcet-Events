03046c69a0aecdee24a80e76ae68306da4e8dab5
Abhishek <ak@localhost.localdomain>
2025-06-12 11:13:37 +0000

    fixed error

--- backend/src/config/supabase.js
+++ backend/src/config/supabase.js
@@ -2,5 +2,6 @@ const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-module.export = supabase;
+module.exports = supabase;
